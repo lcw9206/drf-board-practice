eb896e37b2b16805e69b8fe99c90e4dde7c09b62
Lee <lcw9206@gmail.com>
2018-06-25 23:23:46 +0900

    Create Serializer

---
+++ mysite/board/serializer.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from board.models import Posting, Comment
+from rest_framework import serializers
+
+
+class PostingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Posting
+        fields = ('name', 'title', 'text')
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('posting', 'name', 'text')
