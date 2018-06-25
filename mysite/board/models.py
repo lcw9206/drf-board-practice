f0fcd99a938fd4da94c03524ec80ceaec6b256d0
Lee <lcw9206@gmail.com>
2018-06-25 23:39:30 +0900

    Add __str__ method to Posting, Comment model

--- mysite/board/models.py
+++ mysite/board/models.py
@@ -6,10 +6,14 @@ class Posting(models.Model):
     title = models.CharField(max_length=100, blank=False)
     text = models.TextField()
 
+    def __str__(self):
+        return self.name + ':' + self.title
+
 
 class Comment(models.Model):
     posting = models.ForeignKey(Posting)
     name = models.CharField(max_length=30, blank=False)
     text = models.CharField(max_length=200, blank=False)
 
-
+    def __str__(self):
+        return self.name + ':' + self.text
