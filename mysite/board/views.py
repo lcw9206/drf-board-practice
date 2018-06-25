8f619cf59fead69b02d49b715a5f559a360126c7
Lee <lcw9206@gmail.com>
2018-06-25 23:28:17 +0900

    Add Posting, Comment ViewSet to views.py

--- mysite/board/views.py
+++ mysite/board/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from board.models import Posting, Comment
+from board.serializer import PostingSerializer, CommentSerializer
 
-# Create your views here.
+
+class PostingViewSet(viewsets.ModelViewSet):
+    queryset = Posting.objects.all()
+    serializer_class = PostingSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
