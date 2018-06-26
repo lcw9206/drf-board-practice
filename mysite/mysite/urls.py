c4e4d53857c6c1c239a5469936be4864d4d8175b
Lee <lcw9206@gmail.com>
2018-06-26 23:44:36 +0900

    Add router to urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from board.views import PostingViewSet
 
+
+router = routers.DefaultRouter()
+router.register(r'postings', PostingViewSet)
 
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls'))
 ]
