17d84fce77e37fce8625b62cd8473dd769392c01
Lee <lcw9206@gmail.com>
2018-06-27 23:50:54 +0900

    Add Comments router to urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-from board.views import PostingViewSet
+from board.views import PostingViewSet, CommentViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'postings', PostingViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
