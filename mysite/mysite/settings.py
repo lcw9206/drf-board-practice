6d5bc5e11e94aba72bd866a384ba9f01237425bf
Lee <lcw9206@gmail.com>
2018-07-04 17:38:36 +0900

    Add rest_framework Pagination to settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -113,7 +113,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
 }
 
 
